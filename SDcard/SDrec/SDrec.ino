e5a74f2859be2fe98033bf22454f5595a519bced
malalves <malalves@hotmail.com>
2016-06-10 18:52:30 -0300

    bla

--- SDcard/SDrec/SDrec.ino
+++ SDcard/SDrec/SDrec.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <SD.h>
+#include <Vector.h>
 
 /*para o uno
   ** MOSI - pin 11
@@ -7,29 +8,33 @@
  ** CLK - pin 13
  */
 const int sd_cs = 10;
+bool err;
 
-const int numArq = 2;
-const char *nomes[] = {"dados/a","dados/b"};
-
-class recorder{
+class Recorder{
 	private:
-    File dataWrite;
+		const int numArq = 2;
+		static char *nomes[];
+		File dataWrite;
 		File dataRead;
- 		String data;//teste
+		Vector<int> archiveStatus;
+		String data;//teste
 
 		void dataTreat(){
 
 		}//trata os dados brutos obtidos dos sensores para obter os valores que serão guardados no cartão sd
 
-	public:
-		recorder(){
+	public: 
+		void init(){
 			if (!SD.begin(sd_cs)) {
-    			Serial.println("Card failed, or not present");
+	   			Serial.println("Card failed, or not present");
 				return;
 			}
 			Serial.println("card initialized.");//inicialização do sd
-		}//construtor para ser chamado no setup cria e inicializa o recorder
-
+			for(int i=0; i<numArq; i++){
+				archiveStatus.push_back(0);
+			}
+		}//construtor para ser chamado no setup cria e inicializa o Recorder
+		
 		void rawDataGet(){
 			while(!Serial.available()){
 				;
@@ -37,18 +42,27 @@ class recorder{
 			data = Serial.readString();//teste
 			//dataTreat(); no momento é desnecessauro
 		}//coleta os dados dos sensores
-
+		
 		void data_sd_record(){
 			dataWrite.print(data);
+        	Serial.println(data);
+        	Serial.println("recorded");
+     		String tNom = dataWrite.name();
+      		dataWrite.flush();
 		}//grava os dados no cartão sd
 
 		void data_sd_read(){
 			dataRead.seek(0);
-			String temp = dataRead.readString();
-			Serial.print(temp);
+      		String temp = dataRead.readString();
+      		Serial.println("ok");
+	      	while(temp != NULL){
+       			Serial.println(temp);
+  				temp = dataRead.readString();
+        		Serial.println("read");
+			}
 			Serial.println("fim do arquivo");
 		}
-
+		
 		void dataClear(){
 			File aux;
 			for(int i = 0; i<numArq; i++){
@@ -57,43 +71,54 @@ class recorder{
 				aux.close();
 			}
 		}//limpa os dados gravados no cartão sd
+		
+		bool setFileR(int num){
+			if (archiveStatus[num]) return 1;
 
-		void setFileR(int num){
 			dataRead.close();
 			dataRead = SD.open(nomes[num],FILE_READ);
+			return 0;
 		}//seleciona o arquivo de leitura
 
-		void setFileW(int num){
+		bool setFileW(int num){
+			if (archiveStatus[num]) return 1;
+
 			dataWrite.close();
 			dataWrite = SD.open(nomes[num],FILE_WRITE);
+			return 0;
 		}//seleciona o arquivo de escrita
 
+		void closeR(){
+			dataRead.close();
+		}
+
+		void closeW(){
+			dataWrite.close();
+		}
 };//classe que grava no SD
 
-recorder rec;
+char* Recorder::nomes[]={"dados/a","dados/b"};
+Recorder rec;
 
 void setup(){
 	Serial.begin(9600);
+	rec.init();
 }
 
 void loop(){
-  rec.dataClear();
-	rec.setFileW(0);
-	rec.setFileR(0);
-	Serial.println("digite um dado");
+	//rec.dataClear();
+	Serial.println("ok");
+	err = rec.setFileW(0);
+	if(err){
+		rec.closeR();
+		rec.setFileW(0);
+	}
 	rec.rawDataGet();
 	rec.data_sd_record();
-	rec.data_sd_read();
-	rec.setFileW(1);
-	rec.setFileR(1);
-	Serial.println("digite um dado");
-	rec.rawDataGet();
-	rec.data_sd_record();
-	Serial.println("digite um dado");
-	rec.rawDataGet();
-	rec.data_sd_record();
-	rec.data_sd_read();
-	rec.data_sd_read();
-	rec.dataClear();
+	err = rec.setFileR(0);
+	if(err){
+		rec.closeW();
+		rec.setFileR(0);
+	}
 	rec.data_sd_read();
 }
